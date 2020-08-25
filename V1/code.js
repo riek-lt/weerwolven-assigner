9506f5b172c998eccad4c77e491b92bed28ed854
Riekelt <riekelt@hotmail.com>
2020-08-25 22:44:31 +0200

    Show results on HTML

--- V1/code.js
+++ V1/code.js
@@ -3,12 +3,14 @@ var names = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'];
 var i = 0;
 var number = 0;
 var removed;
+var useTwitch = document.querySelector('#names');
 
 randomize();
 function randomize() {
   for (var i = 0; i < names.length; i++) {
     number = randomIntInc(numbers.length);
     console.log(names[i] + " = " + numbers[number]);
+    useTwitch.innerHTML += names[i] + " = " + numbers[number] + "<br>";
     removed = numbers.splice(number, 1);
   }
 }
