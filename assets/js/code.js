617961bcf82f6bf576f0e9822953841b4afe89d2
Riekelt <riekelt@hotmail.com>
2020-08-25 23:49:21 +0200

    Added role selectors

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,11 +1,12 @@
 var roles = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'];
 var names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
 var i =0;
-var randomNumber = 0;
+var randomNumber = roleNumber = 0;
 var removed;
 var useTwitch = document.querySelector('#names');
+var roleAmount = document.querySelector('#roleAmount');
 
-randomize();
+// randomize();
 
 function randomize() {
   if (names.length !== roles.length) {
@@ -32,3 +33,24 @@ function fillNameTable(i) {
   cell.innerHTML = names[i];
   cell2.innerHTML = roles[randomNumber];
 }
+
+function updateRoleNumber() {
+  roleNumber = getSelectedCheckboxValues('color').length;
+  roleNumber = roleNumber + parseInt(document.getElementById("regWolf").value);
+  roleNumber = roleNumber + parseInt(document.getElementById("regBurg").value);
+roleAmount.innerHTML = "Er zijn nu <b>" + roleNumber + "</b> rollen in het spel.";
+}
+
+function getSelectedCheckboxValues(name) {
+    const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
+    let roleValues = [];
+    checkboxes.forEach((checkbox) => {
+        roleValues.push(checkbox.value);
+    });
+    return roleValues;
+}
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', (event) => {
+    alert(getSelectedCheckboxValues('color'));
+});
