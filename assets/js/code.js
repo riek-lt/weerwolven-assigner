60f2ba2231e08b97a9dc05a13c793f6bd2bfa873
Riekelt <riekelt@hotmail.com>
2020-08-25 22:54:32 +0200

    Put the data in a table

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,16 +1,17 @@
-var numbers = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'];
-var names = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'];
-var i = 0;
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'];
+var names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
+var i = k =0;
 var number = 0;
 var removed;
 var useTwitch = document.querySelector('#names');
 
 randomize();
+
 function randomize() {
   for (var i = 0; i < names.length; i++) {
     number = randomIntInc(numbers.length);
     console.log(names[i] + " = " + numbers[number]);
-    useTwitch.innerHTML += names[i] + " = " + numbers[number] + "<br>";
+    fillNameTable(i);
     removed = numbers.splice(number, 1);
   }
 }
@@ -18,4 +19,12 @@ function randomize() {
 function randomIntInc(high, low) {
   var returnio = Math.floor(Math.random() * high);
   return returnio;
+}
+
+function fillNameTable(i) {
+  row = useTwitch.insertRow(k - 1);
+  cell = row.insertCell(0);
+  cell2 = row.insertCell(1);
+  cell.innerHTML = names[i];
+  cell2.innerHTML = numbers[number];
 }
