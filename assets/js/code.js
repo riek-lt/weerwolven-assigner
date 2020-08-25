5eefdaf2d001448cf00e21415cdbe1978dceff6d
Riekelt <riekelt@hotmail.com>
2020-08-25 23:04:19 +0200

    Refactored variables to make more sense

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,18 +1,22 @@
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'];
+var roles = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'];
 var names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
-var i = k =0;
-var number = 0;
+var i =0;
+var randomNumber = 0;
 var removed;
 var useTwitch = document.querySelector('#names');
 
 randomize();
 
 function randomize() {
+  if (names.length !== roles.length) {
+    useTwitch.innerHTML = "Aantal rollen zijn ongelijk aan het aantal namen."
+    return;
+  }
   for (var i = 0; i < names.length; i++) {
-    number = randomIntInc(numbers.length);
-    console.log(names[i] + " = " + numbers[number]);
+    randomNumber = randomIntInc(roles.length);
+    console.log(names[i] + " = " + roles[randomNumber]);
     fillNameTable(i);
-    removed = numbers.splice(number, 1);
+    removed = roles.splice(randomNumber, 1);
   }
 }
 
@@ -22,9 +26,9 @@ function randomIntInc(high, low) {
 }
 
 function fillNameTable(i) {
-  row = useTwitch.insertRow(k - 1);
+  row = useTwitch.insertRow(-1);
   cell = row.insertCell(0);
   cell2 = row.insertCell(1);
   cell.innerHTML = names[i];
-  cell2.innerHTML = numbers[number];
+  cell2.innerHTML = roles[randomNumber];
 }
